ed7cdcd7156e79e37a3aede299ea9c015c59ed5c
IgorK78 <i.kolegov78@gmail.com>
2023-06-26 00:12:12 +0300

    edit from hw-6

--- src/video.py
+++ src/video.py
@@ -3,19 +3,25 @@ from googleapiclient.discovery import build
 
 class Video:
     API_KEY = os.getenv('API_KEY')
-
     def __init__(self, video_id):
-        self.channel = self.get_channel().videos().list(
-            part='snippet,statistics,contentDetails,topicDetails',
-            id=video_id
-        ).execute()
         self.video_id = video_id
-        self.video_url = 'https://www.youtube.com/' + self.video_id
-        self.video_title = self.channel['items'][0]['snippet']['title']
-        self.video_likes_count = self.channel['items'][0]['statistics']['likeCount']
+        try:
+            self.channel = self.get_channel().videos().list(
+                part='snippet,statistics,contentDetails,topicDetails',
+                id=self.video_id
+            ).execute()
+            self.video_url = 'https://www.youtube.com/' + self.video_id
+            self.title = self.channel['items'][0]['snippet']['title']
+            self.like_count = self.channel['items'][0]['statistics']['likeCount']
+
+        except IndexError:
+            self.title = None
+            self.video_url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
-        return f'{self.video_title}'
+        return f'{self.title}'
 
     def get_channel(self):
         return build('youtube', 'v3', developerKey=self.API_KEY)
